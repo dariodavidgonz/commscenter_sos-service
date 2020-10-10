9a13fe184a64489407d7b5b734f65a8ad742eb43
DarioGonzalez <dariodavidgonz@gmail.com>
2020-10-09 21:59:12 -0300

    Trilateration Unit Tests + MAX_TOLERANCE

--- src/main/java/com/commscenter/topsecret/location/Trilateration2DLocationResolver.java
+++ src/main/java/com/commscenter/topsecret/location/Trilateration2DLocationResolver.java
@@ -8,80 +8,66 @@ import org.springframework.stereotype.Component;
 @Component
 public class Trilateration2DLocationResolver implements LocationResolver {
 
-	public static final Double DELTA_DISTANCE = 10D;
+	public static final Double MAX_TOLERANCE = 10D;
 
 	public SecretPoint getLocation(List<StationLocation> stationLocations) {
+		validateStationLocationSize(stationLocations);
 		List<SecretPoint> points = new ArrayList<>();
 		List<Double> ratios = new ArrayList<>();
 		for (StationLocation stationLocation : stationLocations) {
 			points.add(stationLocation.getCoordinate());
 			ratios.add(stationLocation.getDistance());
 		}
-		SecretPoint trilateratedLocation = doTrilateration(ratios, points);
-		validateTrilateratedLocationAndRatiosWithDelta(ratios, trilateratedLocation, points, DELTA_DISTANCE);
-		return trilateratedLocation;
+		SecretPoint locationResult = doTrilateration(ratios.get(0), ratios.get(1), ratios.get(2), points.get(0), points.get(1),
+				points.get(2));
+		verifyTrilateration(locationResult, ratios.get(0), ratios.get(1), ratios.get(2), points.get(0), points.get(1),
+				points.get(2));
+		return locationResult;
+	}
+
+	private void validateStationLocationSize(List<StationLocation> stationLocations) {
+		if (stationLocations.size() > 3) {
+			throw new LocationNotResolvedException();
+		}		
 	}
 
 	/**
-	 * Validate Trilaterated Location And Ratios With deltaDistance Each distance to
-	 * trilaterated location should be the same +/- deltaDistance.
 	 * 
-	 * @param ratios
-	 * @param points
-	 * @param deltaDistance
+	 * Simple Trilateration based on a 2D Model.
+	 * 
+	 * @param distance1
+	 * @param distance2
+	 * @param distance3
+	 * @param point     a
+	 * @param point     b
+	 * @param point     c
+	 * @return location secret point
 	 */
-	private void validateTrilateratedLocationAndRatiosWithDelta(List<Double> ratios, SecretPoint calculatedLocation,
-			List<SecretPoint> points, double deltaDistance) {
-		for (int i = 0; i < points.size(); i++) {
-			validateDistanceBeetweenPoints(points.get(i), ratios.get(i), calculatedLocation, deltaDistance);
-		}
-
+	private static SecretPoint doTrilateration(Double distance1, Double distance2, Double distance3, SecretPoint pointA,
+			SecretPoint pointB, SecretPoint pointC) {
+		double a = -2 * pointA.getX() + 2 * pointB.getX();
+		double b = -2 * pointA.getY() + 2 * pointB.getY();
+		double c = distance1 * distance1 - distance2 * distance2 - pointA.getX() * pointA.getX()
+				- pointA.getY() * pointA.getY() + pointB.getX() * pointB.getX() + pointB.getY() * pointB.getY();
+		double d = -2 * pointB.getX() + 2 * pointC.getX();
+		double e = -2 * pointB.getY() + 2 * pointC.getY();
+		double f = distance2 * distance2 - distance3 * distance3 - pointB.getX() * pointB.getX()
+				- pointB.getY() * pointB.getY() + pointC.getX() * pointC.getX() + pointC.getY() * pointC.getY();
+		double x = (c * e - f * b) / (e * a - b * d);
+		double y = (c * d - a * f) / (b * d - a * e);		
+		return new SecretPoint(x, y);
 	}
 
-	private void validateDistanceBeetweenPoints(SecretPoint satelliteLocation, double distantToSatellite,
-			SecretPoint calculatedPoint, double deltaDistance) {
-		double distanceToOne = satelliteLocation.calculateDistanceTo(calculatedPoint);
-		double absOne = Math.abs(distantToSatellite - distanceToOne);
-		if (absOne > deltaDistance) {
+	private static void verifyTrilateration(SecretPoint result, Double distance1, Double distance2, Double distance3,
+			SecretPoint pointA, SecretPoint pointB, SecretPoint pointC) {
+		if (!(isPrecisionToPointAccepted(result, distance1, pointA) && isPrecisionToPointAccepted(result, distance2, pointB) && isPrecisionToPointAccepted(result, distance3, pointC))) {
 			throw new LocationNotResolvedException();
 		}
 	}
 
-	/**
-	 * 
-	 * Simple Trilateration based on a 2D Model.
-	 * 
-	 * @param ratios of distances to points
-	 * @param point a
-	 * @param point b
-	 * @param point c
-	 * @return location secret point
-	 */
-	private static SecretPoint doTrilateration(List<Double> ratios, SecretPoint pointA, SecretPoint pointB,
-			SecretPoint pointC) {
-		double a = 2 * pointB.getX() - 2 * pointA.getX();
-		double b = 2 * pointB.getY() - 2 * pointA.getY();
-		double c = ratios.get(0) * ratios.get(0) - ratios.get(1) * ratios.get(1) - pointA.getX() * pointA.getX()
-				+ pointB.getX() * pointB.getX() - pointA.getY() * pointA.getY() + pointB.getY() * pointB.getY();
-		double d = 2 * pointC.getX() - 2 * pointB.getX();
-		double e = 2 * pointC.getY() - 2 * pointB.getY();
-		double f = ratios.get(1) * ratios.get(1) - ratios.get(2) * ratios.get(2) - pointB.getX() * pointB.getX()
-				+ pointC.getX() * pointC.getX() - pointB.getY() * pointB.getY() + pointC.getY() * pointC.getY();
-		double x = (c * e - f * b) / (e * a - b * d);
-		double y = (c * d - a * f) / (b * d - a * e);
-		return new SecretPoint(x, y);
+	private static boolean isPrecisionToPointAccepted(SecretPoint calculatedPoint, Double inputDistanceToRerence, SecretPoint referencePoint) {
+		return Math.abs(calculatedPoint.calculateDistanceTo(referencePoint) - inputDistanceToRerence)  <= MAX_TOLERANCE;
 	}
-	
-	/**
-	 * 
-	 * Simple Trilateration based on a 2D Model.
-	 * 
-	 * @param ratios of distances to points
-	 * @param points
-	 * @return location secret point
-	 */
-	public static SecretPoint doTrilateration(List<Double> ratios, List<SecretPoint> points) {
-		return doTrilateration(ratios, points.get(0), points.get(1), points.get(2));
-	}
-	
+
+
 }
--- src/test/java/com/commscenter/sos/Trilateration2DLocationResolverTest.java
+++ src/test/java/com/commscenter/sos/Trilateration2DLocationResolverTest.java
@@ -20,16 +20,13 @@ import com.commscenter.topsecret.location.Trilateration2DLocationResolver;
 class Trilateration2DLocationResolverTest {
 
 	static Logger logger = LoggerFactory.getLogger(Trilateration2DLocationResolverTest.class);
-
-	final double deltaPrecision = 0.5;
-	
 	private Satellite kenobi = new RebelSatellite(1, "Kenobi", new SecretPoint(-500D, -200D));
 	static Satellite skywalker = new RebelSatellite(2, "Skywalker", new SecretPoint(100D, -100D));
 	static Satellite sato = new RebelSatellite(3, "Sato", new SecretPoint(500D, 100D));
 	
 
 	@Test
-	void testCorrectWithoutDelta() {
+	void testTrilaterationExactValues() {
 		List<StationLocation> stationLocations = new ArrayList<>();
 		stationLocations.add(new StationLocation(kenobi.getLocation(), 316.22776601683796D));
 		stationLocations.add(new StationLocation(skywalker.getLocation(), 300D));
@@ -41,17 +38,41 @@ class Trilateration2DLocationResolverTest {
 	}
 
 	@Test
-	void testCorrectWithDelta() {
+	void testLocationFoundNineDecimalPrecision() {
 		Trilateration2DLocationResolver resolver = new Trilateration2DLocationResolver();
-		List<Double> aux = new ArrayList<Double>();
 		List<StationLocation> stationLocations = new ArrayList<>();
 		stationLocations.add(new StationLocation(kenobi.getLocation(), 815.6501532519932));
 		stationLocations.add(new StationLocation(skywalker.getLocation(), 934.2136653357196));
 		stationLocations.add(new StationLocation(sato.getLocation(), 1125.5643795447686));
 		SecretPoint aux1 = resolver.getLocation(stationLocations);
-		assertEquals(-500.50D, aux1.getX(), deltaPrecision);
-		assertEquals(615.34D, aux1.getY(), deltaPrecision);
+		assertEquals(-500.50D, aux1.getX(), 0.000000001);
+		assertEquals(615.65D, aux1.getY(), 0.000000001);
 	}
+
+	
+	@Test
+	void testInputDistancesRoundedTwoDecimals() {
+		Trilateration2DLocationResolver resolver = new Trilateration2DLocationResolver();
+		List<StationLocation> stationLocations = new ArrayList<>();
+		stationLocations.add(new StationLocation(kenobi.getLocation(), 815.65));
+		stationLocations.add(new StationLocation(skywalker.getLocation(), 934.21));
+		stationLocations.add(new StationLocation(sato.getLocation(), 1125.56));
+		SecretPoint aux1 = resolver.getLocation(stationLocations);
+		assertEquals(-500.50D, aux1.getX(), 0.01);
+		assertEquals(615.65D, aux1.getY(), 0.01);
+	}
+	
+	@Test
+	void testInputDistancesRoundedOneDecimal() {
+		Trilateration2DLocationResolver resolver = new Trilateration2DLocationResolver();
+		List<StationLocation> stationLocations = new ArrayList<>();
+		stationLocations.add(new StationLocation(kenobi.getLocation(), 815.6));
+		stationLocations.add(new StationLocation(skywalker.getLocation(), 934.2));
+		stationLocations.add(new StationLocation(sato.getLocation(), 1125.6));
+		SecretPoint aux1 = resolver.getLocation(stationLocations);
+		assertEquals(-500.50D, aux1.getX(), 0.5);
+		assertEquals(615.65D, aux1.getY(), 0.5);
+	}	
 
 	@Test
 	void testLocationNotResolvedException() {
@@ -60,6 +81,18 @@ class Trilateration2DLocationResolverTest {
 		stationLocations.add(new StationLocation(kenobi.getLocation(), 100D));
 		stationLocations.add(new StationLocation(skywalker.getLocation(), 115.5D));
 		stationLocations.add(new StationLocation(sato.getLocation(), 142.7D));
+		assertThrows(LocationNotResolvedException.class, () -> {
+			resolver.getLocation(stationLocations);
+		});
+	}
+	
+	@Test
+	void testLocationNotResolvedNotIntersectingDistancesException() {
+		Trilateration2DLocationResolver resolver = new Trilateration2DLocationResolver();
+		List<StationLocation> stationLocations = new ArrayList<>();
+		stationLocations.add(new StationLocation(kenobi.getLocation(), 10D));
+		stationLocations.add(new StationLocation(skywalker.getLocation(), 5.5D));
+		stationLocations.add(new StationLocation(sato.getLocation(), 2.7D));
 		List<Double> aux = new ArrayList<Double>();
 		assertThrows(LocationNotResolvedException.class, () -> {
 			resolver.getLocation(stationLocations);
